32fd6e0186fbd27663d150c725985f9a98bfc515
Const4ntineAld <allderenconstantine@gmail.com>
2021-10-11 11:09:09 +0300

    Initial commit

---
+++ GitTest/src/TestClass_Git.java
@@ -0,0 +1,6 @@
+public class TestClass_Git {
+    public static void main(String[] args) {
+        System.out.println("Git! Git! Git! Git!");
+        System.out.println(1 + 9);
+    }
+}
---
+++ GitTest/src/TestProgramm.java
@@ -0,0 +1,11 @@
+public class TestProgramm {
+    public static void main(String[] args) {
+       Boolean isTrue = true;
+        if(isTrue)
+        {
+            System.out.println("======");
+            System.out.println(" :) ");
+            System.out.println("======");
+        }
+    }
+}
